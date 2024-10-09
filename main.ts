f5fa54e10e3d7ecc985b93680b6361655aebf6b1
mark-lutabi <mark.lutabi@mths.ca>
2024-10-09 17:31:19 -0400

    updated my code rock, papper, scissors

--- main.ts
+++ main.ts
@@ -1,8 +1,58 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mark lutabi
+ * Created on: Sep 2024
+ * This program plays rock,papper and scissors.
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber: number = 0
+let score: number = 0
+
+score=0
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = randint(0, 2)
+    basic.clearScreen()
+
+    // if randomNumber was 0 ROCK
+    if (randomNumber == 0) {
+    basic.showIcon(IconNames.SmallSquare)
+        pause(1000)
+        basic.showIcon(IconNames.Happy)
+    }
+   
+
+    // if randomNumber was 1 PAPPER
+    if (randomNumber == 1) {
+    basic.showIcon(IconNames.Square)
+    pause(1000)
+    basic.showIcon(IconNames.Happy)
+    }
+
+    // if randomNumber was 2 SCISSORS
+    if (randomNumber == 2) {
+    basic.showIcon(IconNames.Scissors)
+        pause(1000)
+        basic.showIcon(IconNames.Happy)
+    }
+    
+
+    input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+        score =score + 1
+    })
+
+        input.onButtonPressed(Button.B, function () {
+            basic.clearScreen()
+        basic.showString("Score:" + score)
+      
+
+
+
+   
+})
+})
