3f2958bfee2825bdb0582006268047e77ee7c74e
mark-lutabi <mark.lutabi@mths.ca>
2024-10-10 11:06:25 -0400

    updated code

--- main.ts
+++ main.ts
@@ -1,54 +1,52 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mark lutabi
- * Created on: Sep 2024
- * This program plays rock,paper and scissors.
+ * Created by: Mr. Coxall
+ * Created on: Sep 2020
+ * This program plays rock, papper, scissors.
 */
 
 // variables
 let randomNumber: number = 0
 let score: number = 0
 
-score=0
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
-
 input.onGesture(Gesture.Shake, function () {
     randomNumber = randint(0, 2)
-    basic.clearScreen()
+    basic.clearScreen() 
 
     // if randomNumber was 0 ROCK
     if (randomNumber == 0) {
-    basic.showIcon(IconNames.SmallSquare)
-        pause(5000)
+        basic.showIcon(IconNames.SmallSquare)
+        pause(3000)
         basic.showIcon(IconNames.Happy)
     }
-   
 
     // if randomNumber was 1 PAPER
     if (randomNumber == 1) {
-    basic.showIcon(IconNames.Square)
-    pause(5000)
-    basic.showIcon(IconNames.Happy)
-    }
-
-    // if randomNumber was 2 SCISSORS
-    if (randomNumber == 2) {
-    basic.showIcon(IconNames.Scissors)
-        pause(5000)
+        basic.showIcon(IconNames.Square)
+        pause(3000)
         basic.showIcon(IconNames.Happy)
     }
-    
+    // if randomNumber was 2 SCISSORS
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.Scissors)
+        pause(3000)
+        basic.showIcon(IconNames.Happy)
+    }
 
     input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    basic.showIcon(IconNames.Yes)
-        score =score + 1
+        basic.clearScreen()
+        basic.showIcon(IconNames.Yes)
+        score = score + 1
+        pause(3000)
+        basic.showIcon(IconNames.Happy)
     })
-
-        input.onButtonPressed(Button.B, function () {
-            basic.clearScreen()
+    input.onButtonPressed(Button.B, function () {
+        basic.clearScreen()
         basic.showString("Score:" + score)
-   
-})
+        basic.showIcon(IconNames.Happy)
+
+    })
 })
